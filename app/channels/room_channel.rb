170a276c897fd18252ec8a88e8568ab627e925e1
Reka Kulinyi <reekaa85@gmail.com>
2018-03-07 19:51:58 +0100

    mentions added

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,7 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel"
+    stream_from "room_channel" #global stream
+    stream_from "room_channel_user_#{message_user.id}" # separate stream for users, we send @mention notifications here
   end
 
   def unsubscribed
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,6 @@
 module MessagesHelper
+  #returns HTML from input text using Gihub flavoures markdown
+  def markdown_to_html(text)
+    Kramdown::Document.new(text, input: 'GFM').to_html
+  end
 end
