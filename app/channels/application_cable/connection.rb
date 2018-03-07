b46e8b5a8e63cf0473fdc9a3fb26e9407d503732
Reka Kulinyi <reekaa85@gmail.com>
2018-03-07 16:35:40 +0100

    login protection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    #an instance of the connection class is created when a new connection is made
+    include SessionsHelper
+
+    identified_by :message_user
+
+    def connect
+      self.message_user = find_verified_user
+    end
+
+    private
+    def find_verified_user
+      if logged_in?# comes from SessionsHelper
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
     # Confirms a logged-in user.
     def logged_in_user
-      unless logged_in?
+      unless logged_in? # defined in sessions helper
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
